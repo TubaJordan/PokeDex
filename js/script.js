fe73d4af75f123bd1808f8b598c6aa6912b1082c
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-05-17 19:56:29 -0700

    added in pokemonList with some Pokemon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-alert("Hello World");
-
-let favoriteFood = "Pears";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, weight: 6.9, type: ["grass", "poison"], indexNumber: 1 },
+    { name: "Ivysaur", height: 1, weight: 13, type: ["grass", "poison"], indexNumber: 2 },
+    { name: "Venusaur", height: 2, weight: 100, type: ["grass", "posion"], indexNumber: 3 },
+    { name: "Abra", height: 0.9, weight: 19.5, type: ["psychic"], indexNumber: 63 },
+    { name: "Kadabra", height: 1.3, weight: 56.5, type: ["psychic"], indexNumber: 64 },
+    { name: "Alakazam", height: 1.5, weight: 48, type: ["psychic"], indexNumber: 65 },
+    { name: "Dratini", height: 1.8, weight: 3.3, type: ["dragon"], indexNumber: 147 },
+    { name: "Dragonair", height: 4, weight: 16.5, type: ["dragon"], indexNumber: 148 },
+    { name: "Dragonite", height: 2.2, weight: 210, type: ["dragon", "flying"], indexNumber: 149 }
+];
