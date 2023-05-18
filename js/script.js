b79f9978f7be0aa9429a354c7671a14f740346f6
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-05-17 21:34:18 -0700

    spelling error corrected, key name change to id

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, weight: 6.9, type: ["grass", "poison"], indexNumber: 1 },
-    { name: "Ivysaur", height: 1, weight: 13, type: ["grass", "poison"], indexNumber: 2 },
-    { name: "Venusaur", height: 2, weight: 100, type: ["grass", "posion"], indexNumber: 3 },
-    { name: "Abra", height: 0.9, weight: 19.5, type: ["psychic"], indexNumber: 63 },
-    { name: "Kadabra", height: 1.3, weight: 56.5, type: ["psychic"], indexNumber: 64 },
-    { name: "Alakazam", height: 1.5, weight: 48, type: ["psychic"], indexNumber: 65 },
-    { name: "Dratini", height: 1.8, weight: 3.3, type: ["dragon"], indexNumber: 147 },
-    { name: "Dragonair", height: 4, weight: 16.5, type: ["dragon"], indexNumber: 148 },
-    { name: "Dragonite", height: 2.2, weight: 210, type: ["dragon", "flying"], indexNumber: 149 }
+    { name: "Bulbasaur", height: 0.7, weight: 6.9, type: ["grass", "poison"], id: 1 },
+    { name: "Ivysaur", height: 1, weight: 13, type: ["grass", "poison"], id: 2 },
+    { name: "Venusaur", height: 2, weight: 100, type: ["grass", "poison"], id: 3 },
+    { name: "Abra", height: 0.9, weight: 19.5, type: ["psychic"], id: 63 },
+    { name: "Kadabra", height: 1.3, weight: 56.5, type: ["psychic"], id: 64 },
+    { name: "Alakazam", height: 1.5, weight: 48, type: ["psychic"], id: 65 },
+    { name: "Dratini", height: 1.8, weight: 3.3, type: ["dragon"], id: 147 },
+    { name: "Dragonair", height: 4, weight: 16.5, type: ["dragon"], id: 148 },
+    { name: "Dragonite", height: 2.2, weight: 210, type: ["dragon", "flying"], id: 149 }
 ];
