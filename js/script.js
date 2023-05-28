ca92d76e0a0895d8ddb5591f9223f28815d4712e
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-05-28 02:22:11 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
 
     let pokemonList = [];
-    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=151";
     let modalContainer = document.querySelector("#modal-container");
 
     function addListItem(pokemon) {
